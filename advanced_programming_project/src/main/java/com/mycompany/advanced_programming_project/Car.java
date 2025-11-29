f5ffa715521014b18210d1e01373c928c7c5dd10
Ahmed Belal <ahmedbelal22271@gmail.com>
2025-11-29 17:47:31 +0200

    added the vehicle and its subclasses

---
+++ advanced_programming_project/src/main/java/com/mycompany/advanced_programming_project/Car.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.advanced_programming_project;
+
+/**
+ *
+ * @author marwa
+ */
+public class Car extends Vehicle{
+    String fuelType;
+    Boolean isFamilyCar;
+    int numberOfSeats;
+    
+    Car(String manufacturer,
+            String model,
+            int year,
+            float rentalRatePerDay,
+            int engineCapacity,
+            int numberAvailable,
+            String fuelType,
+            Boolean isFamilyCar,
+            int numberOfSeats
+            ){
+        super();
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.rentalRatePerDay = rentalRatePerDay;
+        this.engineCapacity = engineCapacity;
+        this.numberAvailable = numberAvailable;
+        this.fuelType = fuelType;
+        this.isFamilyCar = isFamilyCar;
+        this.numberOfSeats = numberOfSeats;
+        
+        System.out.println("This is a Car");
+    }
+    
+    void display_info(){
+        System.out.println("this should display the information gui later on");
+    }
+}
