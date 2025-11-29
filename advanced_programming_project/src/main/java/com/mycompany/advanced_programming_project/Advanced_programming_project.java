ff683ab36643e62da2e434cfd9f19002cdc7f580
Ahmed Belal <ahmedbelal22271@gmail.com>
2025-11-29 15:32:44 +0200

    added the java package files

---
+++ advanced_programming_project/src/main/java/com/mycompany/advanced_programming_project/Advanced_programming_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.advanced_programming_project;
+
+/**
+ *
+ * @author marwa
+ */
+public class Advanced_programming_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
