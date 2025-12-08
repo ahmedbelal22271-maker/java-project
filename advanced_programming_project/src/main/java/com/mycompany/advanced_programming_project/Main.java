a05458b09c4433e22a5972b8ebc847ee3da2bd9d
Ahmed Belal <ahmedbelal22271@gmail.com>
2025-12-08 20:27:18 +0200

    added to main, added getters to user

--- advanced_programming_project/src/main/java/com/mycompany/advanced_programming_project/Main.java
+++ advanced_programming_project/src/main/java/com/mycompany/advanced_programming_project/Main.java
@@ -5,6 +5,8 @@
 package com.mycompany.advanced_programming_project;
 
 
+import java.util.Objects;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +15,55 @@ package com.mycompany.advanced_programming_project;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("please enter your email");
+        User[] userArray = {new User(
+                1,
+                "Alice Johnson",
+                "alice.johnson@example.com",
+                "555-123-4567",
+                1234567.89
+        ),
+
+        new User(
+                2,
+                "Mark Stevens",
+                "mark.stevens@example.com",
+                "555-987-6543",
+                9876543.21
+        ),
+
+        new User(
+                3,
+                "Sofia Ramirez",
+                "sofia.ramirez@example.com",
+                "555-777-8888",
+                5678901.34
+        )};
+        Scanner emailInput = new Scanner(System.in);
+        String email = emailInput.nextLine();
+
+        boolean EnteredUser = false;
+
+        while (!EnteredUser){
+            for (User user : userArray){
+                if (Objects.equals(email, user.getEmail())){
+                    System.out.println("Welcome back, " + user.getName());
+                    EnteredUser = true;
+                }
+            }
+            if (!EnteredUser){
+                System.out.println("Invalid email, please try again");
+                email = emailInput.nextLine();
+            }
+        }
+
+        Vehicle[] vehicleArray = {
+                
+        }
+
+
+
+
+
     }
 }
--- advanced_programming_project/src/main/java/com/mycompany/advanced_programming_project/User.java
+++ advanced_programming_project/src/main/java/com/mycompany/advanced_programming_project/User.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.ap;
+package com.mycompany.advanced_programming_project;
 
 /**
  *
@@ -49,5 +49,15 @@ public class User {
 
         }
     }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
 
 }
